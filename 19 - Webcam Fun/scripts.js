42d76fdcaa23b72e8254cf7b8a1e33c2ff06a6ec
Real-Ather <real.ather@gmail.com>
2018-03-12 04:41:36 +0500

    DAY - 19: Webcam FUN - FINISHED

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,73 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  return navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  if (!width || !height) {
+    requestAnimationFrame(paintToCanvas)
+    return
+  }
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0,0, width, height);
+    let pixels = ctx.getImageData(0,0, width, height);
+
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.2;
+
+    ctx.putImageData(pixels, 0, 0);
+    // console.log(pixels);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'Random');
+  link.innerHTHML = `<img src="${data}" alt="WebCam Fun" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] - 20;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return  pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 200] = pixels.data[i + 1];
+    pixels.data[i - 250] = pixels.data[i + 2];
+  }
+  return  pixels;
+}
+
+getVideo();
+paintToCanvas();
+
+video.addEventListener('canplay', paintToCanvas);
